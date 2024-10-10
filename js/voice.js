61fd40c94e55711e77b199cef5a542811fe44586
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2024-10-10 22:44:09 +0300

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -4,15 +4,18 @@ let audioChunks = [];
 export async function startRecording(stream) {
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.start();
+    console.log('Recording started.');
 
     mediaRecorder.addEventListener("dataavailable", event => {
         audioChunks.push(event.data);
+        console.log('Audio chunk available.');
     });
 
     mediaRecorder.addEventListener("stop", () => {
         const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
         const audioUrl = URL.createObjectURL(audioBlob);
         audioChunks = []; // очистка массива для следующей записи
+        console.log('Recording stopped.');
 
         // Здесь можно вызвать функцию для отображения или сохранения аудио
         displayVoiceMessage(audioUrl);
@@ -22,6 +25,7 @@ export async function startRecording(stream) {
 export function stopRecording() {
     if (mediaRecorder) {
         mediaRecorder.stop();
+        console.log('Stopping recording...');
     }
 }
 
@@ -31,4 +35,5 @@ function displayVoiceMessage(audioUrl) {
     audioElement.controls = true;
     audioElement.src = audioUrl;
     voiceMessagesList.appendChild(audioElement);
+    console.log('Voice message displayed.');
 }
