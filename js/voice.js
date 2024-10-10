49ca4aec247acdcc663a1c391c0b1a302f1329ce
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2024-10-10 22:33:22 +0300

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,37 +1,32 @@
-// voice.js
-
 let mediaRecorder;
 let audioChunks = [];
 
-// Начало записи
-function startRecording(stream) {
-    audioChunks = [];
+export async function startRecording(stream) {
     mediaRecorder = new MediaRecorder(stream);
-    
-    mediaRecorder.ondataavailable = (event) => {
-        audioChunks.push(event.data);
-    };
-
-    mediaRecorder.onstop = () => {
-        const audioBlob = new Blob(audioChunks);
-        const audioUrl = URL.createObjectURL(audioBlob);
-        playAudio(audioUrl);
-        // Здесь можно добавить функционал для отправки аудио
-    };
-
     mediaRecorder.start();
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+        audioChunks.push(event.data);
+    });
+
+    mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        audioChunks = []; // очистка массива для следующей записи
+
+        // Здесь можно вызвать функцию для отображения или сохранения аудио
+        displayVoiceMessage(audioUrl);
+    });
 }
 
-// Остановка записи
-function stopRecording() {
+export function stopRecording() {
     mediaRecorder.stop();
 }
 
-// Воспроизведение аудио
-function playAudio(url) {
-    const audio = new Audio(url);
-    audio.play();
+function displayVoiceMessage(audioUrl) {
+    const voiceMessagesList = document.getElementById('voice-messages-list');
+    const audioElement = document.createElement('audio');
+    audioElement.controls = true;
+    audioElement.src = audioUrl;
+    voiceMessagesList.appendChild(audioElement);
 }
-
-// Экспорт функций
-export { startRecording, stopRecording };
