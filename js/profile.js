91ab765ce3584fc3e526b90a4bda0edb0e79496c
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2024-10-10 11:10:59 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,13 @@
+document.getElementById('updateProfile').onclick = function() {
+    const nickname = document.getElementById('nickname').value;
+    document.getElementById('nicknameDisplay').textContent = nickname;
+};
+
 document.getElementById('avatar').onchange = function() {
     const file = this.files[0];
     const reader = new FileReader();
     reader.onload = function(e) {
         document.getElementById('avatarPreview').innerHTML = `<img src="${e.target.result}" alt="Avatar">`;
-    };
+    }
     reader.readAsDataURL(file);
-};
-
-document.getElementById('updateProfile').onclick = function() {
-    const nickname = document.getElementById('nickname').value;
-    alert('Профиль обновлён: ' + nickname);
 };
