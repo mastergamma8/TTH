1a6ebfab25f370f5ec6cc8383cb3c0dc0f9652e6
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2024-10-11 20:06:05 +0300

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleThemeButton = document.getElementById('toggle-theme');
 
+  // Сохранение выбранной темы в локальное хранилище
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+  }
+
   toggleThemeButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
+
+    // Обновление темы в локальном хранилище
+    if (document.body.classList.contains('dark-theme')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
   });
 });
