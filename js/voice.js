b795be21514217ccba79b3a1595b3aa25f1f95b8
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2024-10-12 09:15:09 +0300

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,39 +1,34 @@
-let mediaRecorder;
-let audioChunks = [];
+document.addEventListener('DOMContentLoaded', () => {
+  const recordButton = document.getElementById('record-button');
+  const stopButton = document.getElementById('stop-button');
+  const voiceMessagesList = document.getElementById('voice-messages-list');
 
-export async function startRecording(stream) {
+  let mediaRecorder;
+  let audioChunks = [];
+
+  // Запись голосового сообщения
+  recordButton.addEventListener('click', async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     mediaRecorder = new MediaRecorder(stream);
+    
     mediaRecorder.start();
-    console.log('Recording started.');
+    
+    mediaRecorder.ondataavailable = (event) => {
+      audioChunks.push(event.data);
+    };
 
-    mediaRecorder.addEventListener("dataavailable", event => {
-        audioChunks.push(event.data);
-        console.log('Audio chunk available.');
-    });
+    mediaRecorder.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
+      const audioURL = URL.createObjectURL(audioBlob);
+      const audioElement = document.createElement('audio');
+      audioElement.src = audioURL;
+      audioElement.controls = true;
+      voiceMessagesList.appendChild(audioElement);
+      audioChunks = [];
+    };
 
-    mediaRecorder.addEventListener("stop", () => {
-        const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
-        const audioUrl = URL.createObjectURL(audioBlob);
-        audioChunks = []; // очистка массива для следующей записи
-        console.log('Recording stopped.');
-
-        // Здесь можно вызвать функцию для отображения или сохранения аудио
-        displayVoiceMessage(audioUrl);
-    });
-}
-
-export function stopRecording() {
-    if (mediaRecorder) {
-        mediaRecorder.stop();
-        console.log('Stopping recording...');
-    }
-}
-
-function displayVoiceMessage(audioUrl) {
-    const voiceMessagesList = document.getElementById('voice-messages-list');
-    const audioElement = document.createElement('audio');
-    audioElement.controls = true;
-    audioElement.src = audioUrl;
-    voiceMessagesList.appendChild(audioElement);
-    console.log('Voice message displayed.');
-}
+    stopButton.onclick = () => {
+      mediaRecorder.stop();
+    };
+  });
+});
